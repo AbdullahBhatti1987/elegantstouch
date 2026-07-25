3a16a4c29701d1f276a8aaaf453f07fc9ffbd529
AbdullahBhatti1987 <156850703+AbdullahBhatti1987@users.noreply.github.com>
2026-07-25 10:36:51 +0500

    Update route.js
    remove context from params

--- src/app/api/categories/[id]/route.js
+++ src/app/api/categories/[id]/route.js
@@ -112,7 +112,7 @@ export async function PUT(req, context) {
     await connectDB();
 
     // const { id } = await context.params;
-    const { id } = await context.params;
+    const { id } = await params;
 
     const formData = await req.formData();
 
